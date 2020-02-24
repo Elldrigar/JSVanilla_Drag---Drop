6345119d4d7dbe6b1abc3ea2fe9d1360509fae8b
Artur Gawron <artur@gawron.me>
2020-02-24 19:55:15 +0100

    Add serialNetflix Array

---
+++ app.js
@@ -0,0 +1,15 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const serialsNetflix = [
+  'Stranger Things',
+  'The Umbrella Academy',
+  'Dom z papieru',
+  'Ty',
+  'Sex Education',
+  'Nasza planeta',
+  'Niewiarygodne',
+  'Już nie żyjesz',
+  'Jak nas widzą',
+  'Szkoła dla elity'
+];
